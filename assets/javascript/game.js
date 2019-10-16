8c17f7d2dcf92767d6ba6aa40e70c4b73fb242ba
Bradley Knutson <bradley@bknutson.com>
2019-10-16 16:50:35 -0600

    Added methods for picking a random value out of a list, choosing a category and word for the game, and turning the word into blanks

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,12 +1,35 @@
 
 
 var gameObject = {
+    wordBlanks: [],
+    chosenWord: '',
+    chosenCategory: '',
     wordCategories: {
         cities: ['Denver', "Phoenix", "Amarillo"],
-        foods: ['Donut', 'Sandwich', 'Oatmeal']
+        foods: ['Donut', 'Sandwich', 'Oatmeal'],
+        videoGames: ['Bayonetta', 'Portal', 'Witcher']
     },
-    chooseWord: function(arr){
+    startGame: function(){
+        this.chooseCategory();
+        this.chooseWord();
+        this.makeBlanks(this.chosenWord);
+    },
+    picker: function(arr){
         return arr[Math.floor(Math.random() * arr.length)];
+    },
+    chooseCategory: function(){
+        this.chosenCategory = this.picker(Object.keys(this.wordCategories));
+    },
+    chooseWord: function(){
+        this.chosenWord = this.picker(this.wordCategories[this.chosenCategory]);
+    },
+    makeBlanks: function(str){
+        for(let i = 0; i<str.length; i++){
+            this.wordBlanks[i] = "_";
+        }
+        console.log(this.wordBlanks);
     }
 };
 
+
+gameObject.startGame();
