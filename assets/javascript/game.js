826db713015b99441dbf2b42f8971d90526ca2b0
Bradley Knutson <bradley@bknutson.com>
2019-10-16 18:26:10 -0600

    Added methods for updating the page and modified html to allow for basic updating

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,18 +1,27 @@
 
 
-var gameObject = {
+var guessGame = {
+    wins: 0,
+    losses: 0,
+    guessesLeft: 0,
     wordBlanks: [],
-    chosenWord: '',
+    chosenWord: [],
     chosenCategory: '',
+    guessedLetters: [],
     wordCategories: {
         cities: ['Denver', "Phoenix", "Amarillo"],
         foods: ['Donut', 'Sandwich', 'Oatmeal'],
         videoGames: ['Bayonetta', 'Portal', 'Witcher']
     },
-    startGame: function(){
+    startGame: function(num){
+        this.guessesLeft = num;
         this.chooseCategory();
         this.chooseWord();
         this.makeBlanks(this.chosenWord);
+        this.updatePage();
+    },
+    reset: function(){
+        
     },
     picker: function(arr){
         return arr[Math.floor(Math.random() * arr.length)];
@@ -21,15 +30,39 @@ var gameObject = {
         this.chosenCategory = this.picker(Object.keys(this.wordCategories));
     },
     chooseWord: function(){
-        this.chosenWord = this.picker(this.wordCategories[this.chosenCategory]);
+        let word = this.picker(this.wordCategories[this.chosenCategory]);
+        for(let i = 0; i<word.length; i++){
+            this.chosenWord.push(word[i].toUpperCase());
+        }
     },
-    makeBlanks: function(str){
-        for(let i = 0; i<str.length; i++){
+    makeBlanks: function(arr){
+        for(let i = 0; i<arr.length; i++){
             this.wordBlanks[i] = "_";
         }
-        console.log(this.wordBlanks);
+    },
+    updatePage: function(){
+        $('#wins').text(this.wins);
+
+        $('#losses').text(this.losses);
+
+        for(let i=0; i<this.wordBlanks.length; i++){
+            $('#word').append(this.wordBlanks[i] + ' ');
+        }
+
+        if(guessGame.chosenCategory === 'videoGames'){
+            $('#category').text('Video Games');
+        }else{
+            $('#category').text(guessGame.chosenCategory.charAt(0).toUpperCase() + guessGame.chosenCategory.slice(1));
+        };
+    },
+    letterInWord: function(letter){
+
     }
 };
 
+$(document).ready(function() {
 
-gameObject.startGame();
+    guessGame.startGame(25);
+    console.log(guessGame);    
+
+});
